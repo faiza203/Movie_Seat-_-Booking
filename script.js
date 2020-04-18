3784d29a5a572c2605dd161c487841d7f3820e29
faiza203 <60883181+faiza203@users.noreply.github.com>
2020-04-18 19:45:41 +0500

    improved

---
+++ script.js
@@ -0,0 +1,7 @@
+const main = document.querySelector("#main");
+const seats = document.querySelectorAll(".row .seat");
+const count = document.getElementById("count");
+const total = document.getElementById("total");
+const movieSelect = document.getElementById("movie");
+const ticketPrice = +movieSelect.value;
+console.log(ticketPrice);
